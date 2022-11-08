062cabb843350d2949ad77dac5ef99c3eb6e39aa
Alex Cruz <acruz@qualitrain.com.mx>
2022-11-07 20:02:19 -0600

    ejms de @MethodSource desde clase Interna y de @NullSource

---
+++ src/main/java/qtx/org/negocio/IdInvalidoException.java
@@ -0,0 +1,9 @@
+package qtx.org.negocio;
+
+public class IdInvalidoException extends NegocioException {
+
+	public IdInvalidoException(String message) {
+		super(message);
+	}
+
+}
--- src/test/java/qtx/negocio/CalculadoraComisionesTests.java
+++ src/test/java/qtx/negocio/CalculadoraComisionesTests.java
@@ -3,6 +3,8 @@ package qtx.negocio;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -13,6 +15,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.EnumSource.Mode;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import qtx.org.entidades.Producto;
@@ -20,6 +25,7 @@ import qtx.org.entidades.Servicio;
 import qtx.org.entidades.Temporalidad;
 import qtx.org.negocio.CalculadoraComisiones;
 import qtx.org.negocio.IMovtoComisionable;
+import qtx.org.negocio.IdInvalidoException;
 import qtx.org.negocio.NegocioException;
 
 public class CalculadoraComisionesTests {
@@ -29,7 +35,7 @@ public class CalculadoraComisionesTests {
 	@DisplayName("Tests que van a ser remplazados en la siguiente versión")
 //	@Disabled("Para ver que pasa")
 	class ProductoTestsPorDepreciar {
-	
+		
 	@Test
 	@DisplayName("Calcular comision s/Producto válido, con Utilidad y cant=1")
 	public void testCalcularComisionOK() {
@@ -45,6 +51,27 @@ public class CalculadoraComisionesTests {
 //		assertEquals(50,comision,0.00001,"costo distinto del esperado");
 		assertEquals(50, comision);
 	}
+	
+	@ParameterizedTest
+	@DisplayName("Usando @MethodSource: Calcular comision s/Producto válido, con Utilidad y precio variable")
+	@MethodSource("qtx.negocio.CalculadoraComisionesTests#getPreciosProducto")
+	public void testCalcularComisionVariandoPrecioOK(double precio) {
+		
+		//Dados
+		double costo = 355;
+		Producto producto = new Producto("X-1", "Camisa", precio, costo);
+		double porcComision = 0.10;
+		int cantidad = 1;
+		double comisionEsperada = (precio - costo) * cantidad * porcComision;
+		
+		//Cuando
+		CalculadoraComisiones calculadora = new CalculadoraComisiones(porcComision);
+		double comision = calculadora.calcularComision(producto, cantidad);
+		
+		//Entonces
+		assertEquals(comisionEsperada, comision);
+	}
+	
 	@ParameterizedTest(name = "Iteracion: {index} con porc de comision = {0}")
 	@DisplayName("** Calcular comision s/Producto válido, con Utilidad y comision variable **")
 	@ValueSource(doubles = {0.05,0.1,0.2,0.6,0.5})
@@ -157,7 +184,7 @@ public class CalculadoraComisionesTests {
 	
 	@ParameterizedTest
 	@DisplayName("Comision sobre servicio periodico y cantidad = 10 usando @EnumSource")
-	@EnumSource(Temporalidad.class)
+	@EnumSource(value = Temporalidad.class, names = { "ANUAL", "MENSUAL" }, mode = Mode.EXCLUDE)
 	public void testCalcularComision_servicioPeriodico_cant10_B(Temporalidad temporalidadI) {
 		
 		//Dados: Valores usados en la prueba
@@ -258,6 +285,32 @@ public class CalculadoraComisionesTests {
 		
 		//Entonces: Validación de resultados esperados vc obtenidos
 		assertThrows(NegocioException.class, funcion ,"No lanza la excepcion esperada");
+	}
+	
+	@ParameterizedTest(name="iteracion {index}, valor probado: [{0}]")
+	@DisplayName("Comision sobre servicio con id inválido")
+	@NullAndEmptySource
+	@ValueSource(strings = {"   ", "\t", "\t "})
+	@Disabled
+	public void testCalcularComision_servicioAnual_idInvalido(String id) {
+		//Dados: Valores usados en la prueba
+		Servicio servicio = new Servicio(id,"Streaming HBO", 50);
+		
+		//Cuando: Código a evaluar
+		IMovtoComisionable posibleVenta = servicio.getIMovtoComisionable(Temporalidad.BIMESTRAL, 1);
+		CalculadoraComisiones calculadora = new CalculadoraComisiones(0.05);
+		Executable funcion = () -> calculadora.calcularComision(posibleVenta);
+		
+		//Entonces: Validación de resultados esperados vc obtenidos
+		assertThrows(IdInvalidoException.class, funcion ,"No lanza la excepcion esperada");
+	}
+	public static Stream<Double> getPreciosProducto(){
+		return Stream.of(
+				(Double)600.0,
+				(Double)600.80, 
+				(Double)750.0, 
+				(Double)1500.0, 
+				(Double)1150.0);
 	}
 	
 }
