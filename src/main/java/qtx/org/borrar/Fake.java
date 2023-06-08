40e98e68e0fde94e69d5cb7802f594972635c0d6
Alex Cruz <acruz@qualitrain.com.mx>
2023-06-08 17:40:31 -0500

    prueba de integracion de cambios con github

---
+++ src/main/java/qtx/org/borrar/Fake.java
@@ -0,0 +1,19 @@
+package qtx.org.borrar;
+
+public class Fake {
+	private String dato1;
+	private String dato2;
+	
+
+	public Fake(String dato1, String dato2) {
+		super();
+		this.dato1 = dato1;
+		this.dato2 = dato2;
+	}
+
+
+	public Fake() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
